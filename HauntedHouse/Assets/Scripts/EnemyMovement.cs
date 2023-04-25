4f152e391d17dbd2cab9269e72dcb707b24011f0
21546810 <127229781+21546810@users.noreply.github.com>
2023-04-25 18:08:42 +0100

    Changed Enemy movement and end level door
    I have functioned the enemy to attack player and also functioned the last door to take player to the main menu.

--- HauntedHouse/Assets/Scripts/EnemyMovement.cs
+++ HauntedHouse/Assets/Scripts/EnemyMovement.cs
@@ -3,16 +3,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.SceneManagement;
 
 public class EnemyMovement : MonoBehaviour
 {
-    private void Start()
-    {
-        
-    }
+    public Transform playerTransform;
+    public float triggerDistance;
+    public float enemySpeed;
 
     private void Update()
     {
-        
+        float step = enemySpeed * Time.deltaTime;
+
+        if (Vector2.Distance(gameObject.transform.position, playerTransform.position) < triggerDistance)
+            transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, step);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == playerTransform.gameObject.name)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
